f9dcd935bf1c8735e090e630ffbd38f7495cbd30
Ferry Yudhitama <ferry.yudhitama@karmagroup.com>
2025-05-27 02:30:20 +0800

    update new

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,4 +41,21 @@ $( document ).ready(function() {
             e.preventDefault();
              owlDiscover.trigger('next.owl.carousel');
         })
+
+
+        //accordion
+            const headers = document.querySelectorAll(".accordion-header");
+                headers.forEach(header => {
+                header.addEventListener("click", () => {
+                    const item = header.parentElement;
+                    item.classList.toggle("active");
+
+                    // Optional: close others
+                    document.querySelectorAll(".accordion-item").forEach(i => {
+                    if (i !== item) {
+                        i.classList.remove("active");
+                    }
+                    });
+                });
+            });
 });
